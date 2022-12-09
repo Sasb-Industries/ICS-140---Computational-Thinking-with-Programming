b4f406e6ddc1a5b86f6c25e535916b118b60813c
Sasha Johnson <sashkajohnson@gmail.com>
2022-12-09 16:26:54 -0600

    Added Player Logic function

--- blackjack.py
+++ blackjack.py
@@ -3,14 +3,13 @@ import random
 
 def main():
     print("\n" "Welcome to Black Jack !")
-    input("Press any key to be dealt a hand.")
+    input("Press any key to be dealt a hand.\n")
     round()
 
 def round():
     shuffled_deck = shuffle_deck(make_deck())
     player_hand, dealer_hand = deal_cards(shuffled_deck)
-    print(f"You got dealt a(n) {player_hand[0]} and a(n) {player_hand[1]} .")
-    draw_2cards(player_hand)
+    player_logic(player_hand)
     print(dealer_hand)
     # draw_card(dealer_hand)
 
@@ -42,6 +41,7 @@ def deal_single_card(deck):
     del deck[0]
     return card
 
+# will draw the first two cards the user gets
 def draw_2cards(hand):
             print(f" _______    _______\n"
                  f"| {hand[0]}     |  | {hand[1]}     |\n"
@@ -49,4 +49,11 @@ def draw_2cards(hand):
                  f"|       |  |       |\n"
                  f"|     {hand[0]} |  |     {hand[1]} |\n"
                  f"|_______|  |_______|")
+
+# def draw_3cards(hand): draws 3 cards similar to the draw_2cards() function
+
+def player_logic(hand):
+    print(f"You got dealt a(n) {hand[0]} and a(n) {hand[1]} .")
+    draw_2cards(hand)
+
 main()

a6c4566cbdccc49d6201de8f419b4e83e343e2fe
Sasha Johnson <sashkajohnson@gmail.com>
2022-12-10 20:02:38 -0600

    - Completed Player Logic - Deleting .DS_Store files again

--- blackjack.py
+++ blackjack.py
@@ -10,6 +10,7 @@ def round():
     shuffled_deck = shuffle_deck(make_deck())
     player_hand, dealer_hand = deal_cards(shuffled_deck)
     player_logic(player_hand, shuffled_deck)
+    dealer_logic(dealer_hand, shuffled_deck)
     print(dealer_hand)
     # draw_card(dealer_hand)
 
@@ -41,6 +42,65 @@ def deal_single_card(deck):
     del deck[0]
     return card
 
+def player_logic(hand, deck):
+    print(f"You got dealt a(n) {hand[0]} and a(n) {hand[1]} .")
+    draw_2cards(hand)
+    total = 0
+    stay = True
+    while total < 22 and stay:
+        hit_stay = input("Would you like to hit or stay? ").lower()
+        while hit_stay != "hit" and hit_stay != "stay":
+            print(hit_stay)
+            hit_stay = input("Invalid input. Would you like to hit or stay? ").lower()
+        if hit_stay == "hit":
+            hit(hand, deck)
+            total = get_hand_value(hand)
+            if total > 21:
+                print("You Busted !")
+        else:
+            print(f" Your hand is : {total}")
+            stay = False
+
+def dealer_logic(hand, deck):
+    print(f"The dealer has a(n) {hand[0]} and a(n) {hand[1]} .")
+    draw_2cards(hand)
+    total = get_hand_value(hand)
+    while total < 17:
+        hit(hand, deck)
+        total = get_hand_value(hand)
+    # stay = True
+    # while total < 22 and stay:
+    #     if total < 17:
+    #         hit(hand, deck)
+    #         total = get_hand_value(hand, total)
+    #         if total > 21:
+    #             print("You Busted !")
+    #     else:
+    #         print(f" Your hand is : {total}")
+    #         stay = False
+
+def hit(hand, deck):
+    hand.append(deck.pop())
+    if len(hand) == 3:
+        draw_3cards(hand)
+    elif len(hand) == 4:
+        draw_4cards(hand)
+    else:
+        print("More than 4 cards in hand !")
+
+def get_hand_value(hand):
+    total = 0
+    for card in hand:
+        if card in ["J","Q","K"]:
+            total += 10
+        elif card == "A":
+            total += 1
+        elif 1 < card < 11:
+            total += card
+        else:
+            print("Card Not Found !") 
+    return total
+
 # will draw the first two cards the user gets
 def draw_2cards(hand):
             print(f" _______    _______\n"
@@ -59,21 +119,12 @@ def draw_3cards(hand):
                 f"|     {hand[0]} |  |     {hand[1]} |  |     {hand[2]} |\n"
                 f"|_______|  |_______|  |_______|\n")
 
-def player_logic(hand, deck):
-    print(f"You got dealt a(n) {hand[0]} and a(n) {hand[1]} .")
-    draw_2cards(hand)
-    hit_stay = input("Would you like to hit or stay? ").lower()
-    while hit_stay != "hit" and hit_stay != "stay":
-        print(hit_stay)
-        hit_stay = input("Invalid input. Would you like to hit or stay? ").lower()
-    if hit_stay == "hit":
-        hit(hand, deck)
-
-def hit(hand, deck):
-    hand.append(deck.pop())
-    draw_3cards(hand)
-    
-
-
+def draw_4cards(hand):
+            print(f" _______    _______    _______    _______\n"
+                f"| {hand[0]}     |  | {hand[1]}     |  | {hand[2]}     |  | {hand[3]}     |\n"
+                f"|       |  |       |  |       |  |       |\n"
+                f"|       |  |       |  |       |  |       |\n"
+                f"|     {hand[0]} |  |     {hand[1]} |  |     {hand[2]} |  |     {hand[3]} |\n"
+                f"|_______|  |_______|  |_______|  |_______|\n")
 
 main()
